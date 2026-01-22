d17d2248d7402e6f77aaeca6c93a5e7659c3f908
Martin Eisemann <eisemann@MacBook-Pro-von-Martin.local>
2026-01-22 20:45:45 +0100

    Change indentation to 4 for c and cpp files. Add some explanation on CLI tools to README.md

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ My own nvim configuration
 
 In the best case you only need to copy the nvim folder to your ~/.config/ folder.
 
+If you encounter errors these are most likely because the CLI tools might be missing on your system, e.g., if you have problems with the treesitter plugin, make sure the treesitter-CLI tools are installed. Ask your favourite AI on how to do that for your system.
+
 Have fun!
 
 Martin
--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -4,8 +4,17 @@ return {
         formatters_by_ft = {
             lua = { "stylua" },
             python = { "black" },
+            -- Only list the formatter name here
             cpp = { "clang-format" },
             c = { "clang-format" },
+        },
+        -- Define the custom behavior for clang-format here
+        formatters = {
+            ["clang-format"] = {
+                prepend_args = {
+                    "--style={BasedOnStyle: LLVM, IndentWidth: 4}",
+                },
+            },
         },
         format_on_save = {
             timeout_ms = 500,
